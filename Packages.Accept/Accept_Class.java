e288916749b3a92089c0b94343cf9b332c78657b
Girjesh <107177466+GirjeshBaghel@users.noreply.github.com>
2022-07-20 15:49:28 +0530

    Create Accept_Class.java

---
+++ Packages.Accept/Accept_Class.java
@@ -0,0 +1,16 @@
+package Packages.Accept;
+
+import java.util.Scanner;
+
+public class Accept_Class {
+	
+	public static Scanner sc = new Scanner(System.in);
+	public  static int a,b;
+	public static void accept()
+	{		
+		System.out.print("Enter the two no : ");
+		a = sc.nextInt();
+		b = sc.nextInt();
+	}
+
+}
