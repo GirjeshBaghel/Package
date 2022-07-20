c4948c220f768dd4bf4127287f290fc971487767
Girjesh <107177466+GirjeshBaghel@users.noreply.github.com>
2022-07-20 15:43:08 +0530

    Create Calling_Class.java

---
+++ Packages.Calling/Calling_Class.java
@@ -0,0 +1,19 @@
+package Packages.Calling;
+
+import Packages.Ternary.Ternary_Class;
+
+public class Calling_Class extends Ternary_Class {
+
+	public static void main(String[] args)
+	{
+		Ternary_Class ob = new Ternary_Class();
+		ob.arth();
+		ob.assig();
+		ob.con();
+		ob.logi();
+		ob.bit();
+		ob.ternary();
+		
+	}		
+		
+}
