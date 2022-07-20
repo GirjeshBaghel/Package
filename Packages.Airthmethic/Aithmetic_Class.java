8add1db10588da11288333ea312dadd403dfb86e
Girjesh <107177466+GirjeshBaghel@users.noreply.github.com>
2022-07-20 15:48:37 +0530

    Create Aithmetic_Class.java

---
+++ Packages.Airthmethic/Aithmetic_Class.java
@@ -0,0 +1,19 @@
+package Packages.Airthmethic;
+
+import Packages.Accept.Accept_Class;
+
+public class Aithmetic_Class extends Accept_Class {
+	
+
+	public  void arth()
+	{
+		System.out.println("This is Airthmetic Package ");
+		Accept_Class.accept();
+		System.out.println("Sum : "+(a+b));
+		System.out.println("Sub : "+(a-b));
+		System.out.println("Mul : "+(a*b));
+		System.out.println("Div : "+(a/b));
+		System.out.println("Mod : "+(a%b));
+	}	
+
+}
