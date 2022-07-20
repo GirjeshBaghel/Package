ffa79113fd980326231ae7e1089fa40d4810c519
Girjesh <107177466+GirjeshBaghel@users.noreply.github.com>
2022-07-20 15:44:27 +0530

    Create Ternary_Class.java

---
+++ Packages.Ternary/Ternary_Class.java
@@ -0,0 +1,16 @@
+package Packages.Ternary;
+
+import Packages.Bitwise.Bitwise_Class;
+
+public class Ternary_Class  extends Bitwise_Class{
+	
+	public void ternary()
+	{
+		
+        System.out.println("\nTernary Package : ");
+        accept();
+        int c = (a>b)? a:b;
+        System.out.print(c);
+	}
+
+}
